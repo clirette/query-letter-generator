e330e86c2be9835a53850322d56ed138c121ba31
Chase Lirette <chaselirette@yahoo.com>
2019-06-05 23:03:36 -0500

    Finished 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Header from "./Header";
+import logo from "../images/logo.png";
 
 const NotFound = () => {
-  return <div>404 not found</div>;
+  return (
+    <div className="not-found">
+      <Header />
+      <div className="not-found-container">
+        <div className="not-found__titles">
+          <h1 className="not-found__title">404</h1>
+          <h1 className="not-found__title">Page Not Found</h1>
+        </div>
+        <div className="images-container">
+          <img
+            src={logo}
+            alt="Query Generator Logo"
+            className="not-found__logo not-found__logo--1"
+          />
+          <img
+            src={logo}
+            alt="Query Generator Logo"
+            className="not-found__logo not-found__logo--2"
+          />
+          <img
+            src={logo}
+            alt="Query Generator Logo"
+            className="not-found__logo not-found__logo--3"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
